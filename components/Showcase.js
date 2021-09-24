a1327da5762dc6d8da7d1fe82d494779653c639f
Sagdi Formanov <forsi1@yahoo.com>
2021-09-24 15:20:35 +0500

    relative paths, presentational components

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to Adopt uz project</h1>
+      <h2>Find missing or Adopt your new pet</h2>
+    </div>
+  )
+}
