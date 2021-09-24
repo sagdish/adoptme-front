e5e5a7cd8b9dce6057fb60cddc94a52cd5e1c8b1
Sagdi Formanov <forsi1@yahoo.com>
2021-09-24 14:41:28 +0500

    custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+import {FaExclamationTriangle} from 'react-icons/fa'
+import Link from 'next/link'
+import Layout from '../components/Layout'
+import styles from '../styles/404.module.css'
+
+export default function NotFoundPage() {
+  return (
+    <Layout title='Page Not Fount'>
+      <div className={styles.error}>
+        <h1>
+          <FaExclamationTriangle /> 404
+        </h1>
+        <h4>Soryan brat ili sistra, netu da takoy stranica</h4>
+        <Link href='/'>Айда домой</Link>
+      </div>
+    </Layout>
+  )
+}
